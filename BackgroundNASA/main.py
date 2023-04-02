004e2dfcb12baa312ae0974fd1c39d757a434885
KriZa96 <78443368+KriZa96@users.noreply.github.com>
2023-04-02 20:40:22 +0200

    Add files via upload

---
+++ BackgroundNASA/main.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import os
+from datetime import datetime
+from datetime import timedelta
+from random import randint
+
+
+""" Rjesiti problem ako je danas video i skinuli smo neki fajl proslog datuma da ne skida ponovo neki random, 
+ rjesit problem brisanja iz pic of the day prijasnjeg file-a"""
+
+
+def get_data(api_key, local_date):
+    raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={local_date}').text
+    response = json.loads(raw_response)
+    if is_image(response):
+        return response
+    else:
+        raise ValueError
+
+
+def get_title(response):
+    return response['title']
+
+
+def get_date(response):
+    return response['date']
+
+
+def get_explaination(response):
+    return response['explanation']
+
+
+def get_hdurl(response):
+    return response['hdurl']
+
+
+def download_image(url, date):
+    raw_image = requests.get(url).content
+    with open(f'PicOfTheDay\\{date}.jpg', 'wb') as file:
+        file.write(raw_image)
+
+
+def is_image(response):
+    return response["media_type"] == 'image'
+
+
+def is_leap_year(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def is_file_present(date, path='PicOfTheDay'):
+    return os.path.isfile(f'{path}\\{date}.jpg')
+
+
+def new_date():
+    """Sets random date"""
+    start_year = 1996
+    year = randint(start_year, datetime.now().year)
+    month = randint(1, 12)
+    if month == 2:
+        day = randint(1, 28 if not is_leap_year(year) else 29)
+    elif month in [4, 6, 9, 11]:
+        day = randint(1, 30)
+    else:
+        day = randint(1, 31)
+
+    final_year = str(year)
+    final_month = ('0' + str(month)) if len(str(month)) == 1 else month
+    final_day = ('0' + str(day)) if len(str(day)) == 1 else day
+
+    return f'{final_year}-{final_month}-{final_day}'
+
+
+if __name__ == '__main__':
+    key = 'DEMO_KEY'
+    local_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    # local_date = '2023-02-18'
+    while not is_file_present(local_date):
+        try:
+            data = get_data(key, local_date)
+            picture_url = get_hdurl(data)
+        except (KeyError, ValueError):
+            local_date = new_date()
+            continue
+        download_image(picture_url, local_date)
